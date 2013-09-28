872fd7aaaaacd67f40218588d7d9c939b540406d
Joe Burgess <joe@flatironschool.com>
2013-09-28 12:33:33 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JawboneHack::Application.config.secret_token = 'e3a49babc690714dd3a096de7aed972fdffcf6366395764164c8a63a518cace12cf48c651512fce8336f5659f205b20177e91c6ddcf7732e8d4c5f882a615b88'
